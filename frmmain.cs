48f4eaa1b85a66e468f471f97d0b2048409b099b
quanttm-01 <79899179+quanttm-01@users.noreply.github.com>
2021-10-27 13:55:41 +0700

    Add files via upload

---
+++ frmmain.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QL_Banhang
+{
+    public partial class frmmain : Form
+    {
+        public frmmain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmmain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kếtThúcChươngTrìnhToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void helloWorldToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form f = new frmHello();
+            toolStripStatusLabel1.Text = "Hello world";
+            f.ShowDialog();
+            toolStripStatusLabel1.Text = "Ready!";
+        }
+
+        private void cộngSốChẵnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form f = new FrmBT1();
+            f.ShowDialog();
+        }
+
+        private void máyTínhBỏTúiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form f = new frmMT();
+            f.ShowDialog();
+        }
+
+        private void kếtThúcToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            Form f = new frmHello();
+            f.Show();
+        }
+
+        private void toolStripSplitButton1_ButtonClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
